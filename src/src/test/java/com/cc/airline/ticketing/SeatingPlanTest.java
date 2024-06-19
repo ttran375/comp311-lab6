e135e3b8b3f1b414d7edb49e2b24549757fcc5df
ttran375 <ttran375@my.centennialcollege.ca>
2024-06-19 23:45:33 +0000

    Setup environment

---
+++ src/src/test/java/com/cc/airline/ticketing/SeatingPlanTest.java
@@ -0,0 +1,57 @@
+package com.cc.airline.ticketing;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SeatingPlanTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		System.out.println(
+				"Starting test of the SeatingPlan default constructor:setUpBeforeClass()");
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		System.out.println(
+				"Starting test of the SeatingPlan default constructor:setUpBeforeClass():tearDownAfterClass()");
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		System.out.println(
+				"Starting test of the SeatingPlan default constructor:setUpBeforeClass():setUp()");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println(
+				"Starting test of the SeatingPlan default constructor:setUpBeforeClass():tearDown()");
+	}
+
+	@Test
+	public void testSeatingPlan() {
+		// fail("Not yet implemented");
+
+		SeatingPlan sp = new SeatingPlan();
+		// assertNotNULL(sp);
+
+		int actualNumBusSeats = sp.getNumBusSeats();
+		int actualNumTotalRows = sp.getTotalRows();
+		int actualNumTotalSeats = sp.getTotalSeats();
+
+		int expectedNumBusSeats = 2;
+		int expectedNumTotalRows = 4;
+		int expectedNumTotalSeats = 14;
+
+		assertEquals(expectedNumBusSeats, actualNumBusSeats);
+		assertEquals(expectedNumTotalRows, actualNumTotalRows);
+		assertEquals(expectedNumTotalSeats, actualNumTotalSeats);
+	}
+
+}
